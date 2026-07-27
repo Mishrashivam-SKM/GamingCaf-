72201641f4b5300f091418013916e5b263f1e12f
boiboiboing <2024.shivamm@gmail.com>
2026-07-28 00:06:45 +0530

    IDK

--- omni-command-frontend/src/components/common/PremiumInput.tsx
+++ omni-command-frontend/src/components/common/PremiumInput.tsx
@@ -1,4 +1,4 @@
-import React, { InputHTMLAttributes } from 'react';
+import React, { type InputHTMLAttributes } from 'react';
 
 interface PremiumInputProps extends InputHTMLAttributes<HTMLInputElement> {
   label?: string;
